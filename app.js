f44befcaaa36f1693c8ef45533b81bb8bd07e7f6
JYOTIRANJAN MOHANTY <107472728+jyotiranjan1997@users.noreply.github.com>
2024-01-01 21:07:07 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const { MongoClient } = require('mongodb');
+const cors = require("cors");
+const PORT = process.env.PORT;
+const app = express();
+const { connect } = require("./src/Config/db");
+const { UserRoute } = require("./src/Routes/UserRoute");
+const { MoviesRoute } = require("./src/Routes/MoviesRoutes");
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/user", UserRoute);
+app.use("/api/movie", MoviesRoute);
+
+app.listen(PORT, async () => {
+  await connect();
+  console.log("listening at " + PORT);
+});
+
+// client.connect(err => {
+//   if(err){ console.error(err); return false;}
+//   // connection to mongo is successful, listen for requests
+//   app.listen(PORT, () => {
+//       console.log("listening for requests");
+//   })
+// });
