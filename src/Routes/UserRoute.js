6bc3459e76e9f54b71fdda02ad5def061dd7cc00
JYOTIRANJAN MOHANTY <107472728+jyotiranjan1997@users.noreply.github.com>
2024-01-02 00:05:59 +0530

    Add files via upload

--- src/Routes/UserRoute.js
+++ src/Routes/UserRoute.js
@@ -1,17 +1,18 @@
 const express = require("express");
+// const { verifyadmin } = require("../Middleware/verifyadmin");
 const {
   User_Login,
   User_Signup,
   Update_User,
   User_Lists,
 } = require("../Controller/UserController");
-const { verifyadmin } = require("../Middleware/verifyadmin");
+
 const UserRoute = express.Router();
 
 UserRoute.post("/login", User_Login); // TO LOGIN USER
-UserRoute.put("/", verifyadmin, Update_User); // TO Update
-UserRoute.post("/register", User_Signup); // To Register user with admin validation
-UserRoute.get("/", verifyadmin, User_Lists); // TO get the list
-UserRoute.get("/:id", verifyadmin, User_Lists); // To get single record for update
+// UserRoute.put("/", verifyadmin, Update_User); // TO Update
+// UserRoute.post("/register", User_Signup); // To Register user with admin validation
+// UserRoute.get("/", verifyadmin, User_Lists); // TO get the list
+// UserRoute.get("/:id", verifyadmin, User_Lists); // To get single record for update
 
 module.exports = { UserRoute };
