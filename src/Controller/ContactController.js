8b35ef26aaa0012fb648e7b36c70ac0277e07a47
JYOTIRANJAN MOHANTY <107472728+jyotiranjan1997@users.noreply.github.com>
2024-01-01 23:42:56 +0530

    Add files via upload

--- src/Controller/ContactController.js
+++ src/Controller/ContactController.js
@@ -3,7 +3,7 @@ const { Contact } = require("../Models/ContactModel")
 
 
 const CREATE_CONTACT_CONTROLLER=async(req,res)=>{
-    console.log(req.body)
+   
     try{
         await Contact.create(req.body);
         res.status(200).json({Result:"Thanks for contact us ! We will get back to you soon."})
